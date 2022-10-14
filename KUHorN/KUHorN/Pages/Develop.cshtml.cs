10ebf8efe010ae4103fda73a312e541b84aa6162
Spencer Sliffe <spenceronw@ku.edu>
2022-10-14 03:16:30 -0500

    This Is The Full Website As I have it so far

---
+++ KUHorN/KUHorN/Pages/Develop.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KUHorN.Pages
+{
+    public class DevelopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
